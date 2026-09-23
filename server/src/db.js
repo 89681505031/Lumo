324f70080ece135dd126de0910ce8816e6697b76
Alex <alexpimenov98gg@gmail.com>
2026-09-23 17:55:37 +0900

    Verify PostgreSQL TLS certificates by default

--- server/src/db.js
+++ server/src/db.js
@@ -2,7 +2,7 @@ import pg from "pg";
 const { Pool } = pg;
 export const pool = process.env.DATABASE_URL ? new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DATABASE_SSL === "false" ? false : { rejectUnauthorized: false },
+  ssl: process.env.DATABASE_SSL === "false" ? false : { rejectUnauthorized: process.env.DATABASE_SSL_REJECT_UNAUTHORIZED !== "false" },
   max: 5,
   connectionTimeoutMillis: 5000,
   idleTimeoutMillis: 10000,
