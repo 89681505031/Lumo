3f18b3a740f8de9df88774ce6a51bd29b8a85116
Alex <alexpimenov98gg@gmail.com>
2026-09-26 02:34:49 +0900

    Add Material icons for flat Lumo UI

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -75,6 +75,7 @@ dependencies {
  implementation(platform("androidx.compose:compose-bom:2025.01.01"))
  implementation("androidx.activity:activity-compose:1.10.0")
  implementation("androidx.compose.material3:material3")
+ implementation("androidx.compose.material:material-icons-extended")
  implementation("androidx.compose.ui:ui")
  implementation("androidx.compose.animation:animation-core")
  implementation("com.squareup.okhttp3:okhttp:4.12.0")
