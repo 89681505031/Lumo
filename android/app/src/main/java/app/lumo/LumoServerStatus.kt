3237783953a9071192b8d15334b7470a99b99691
Alex <alexpimenov98gg@gmail.com>
2026-09-26 00:28:10 +0900

    Show account security capabilities in server status

--- android/app/src/main/java/app/lumo/LumoServerStatus.kt
+++ android/app/src/main/java/app/lumo/LumoServerStatus.kt
@@ -22,7 +22,9 @@ private data class LumoServerSnapshot(
     val reactionsReady:Boolean,
     val callsReady:Boolean,
     val turnReady:Boolean,
-    val mediaReady:Boolean
+    val mediaReady:Boolean,
+    val sessionSecurityReady:Boolean,
+    val passwordChangeReady:Boolean
 )
 
 private fun fetchLumoServerSnapshot():LumoServerSnapshot {
@@ -53,7 +55,9 @@ private fun fetchLumoServerSnapshot():LumoServerSnapshot {
         reactionsReady=caps.optBoolean("groupReactions",false),
         callsReady=caps.optBoolean("callsReady",false),
         turnReady=caps.optBoolean("turnReady",false),
-        mediaReady=caps.optBoolean("mediaReady",false)
+        mediaReady=caps.optBoolean("mediaReady",false),
+        sessionSecurityReady=caps.optBoolean("sessionRevokeOthers",false),
+        passwordChangeReady=caps.optBoolean("passwordChange",false)
     )
 }
 
@@ -116,6 +120,8 @@ fun LumoServerStatusCard(){
             ServerStatusRow("Сигнализация звонков",s.callsReady)
             ServerStatusRow("TURN для аудио/видео",s.turnReady)
             ServerStatusRow("Фото, видео и файлы",s.mediaReady)
+            ServerStatusRow("Безопасность сессий",s.sessionSecurityReady)
+            ServerStatusRow("Смена пароля",s.passwordChangeReady)
         }
         if(error.isNotBlank()){
             Spacer(Modifier.height(8.dp))
